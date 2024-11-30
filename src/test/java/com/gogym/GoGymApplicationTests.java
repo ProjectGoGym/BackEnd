8cb5dc7ef316bf71f45bfd5cbf891fcc0fb0dcfb
cwhite723 <c_white723@naver.com>
2024-12-01 00:47:38 +0900

    📝 docs: 배포 관련 파일 추가

--- src/test/java/com/gogym/GoGymApplicationTests.java
+++ src/test/java/com/gogym/GoGymApplicationTests.java
@@ -1,13 +1,17 @@
 package com.gogym;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GoGymApplicationTests {
 
+  /*
 	@Test
 	void contextLoads() {
-	}
-
+	}*/
 }
+
