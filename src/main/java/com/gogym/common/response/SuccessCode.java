8477b5a73f0cc8450c4010927aef4509ca553333
BaekSeungHyun7 <sergeant_keroro@naver.com>
2024-11-30 19:09:45 +0900

    remove : 주석 제거

--- src/main/java/com/gogym/common/response/SuccessCode.java
+++ src/main/java/com/gogym/common/response/SuccessCode.java
@@ -3,7 +3,7 @@ package com.gogym.common.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
-//
+
 @Getter
 @AllArgsConstructor
 public enum SuccessCode implements ResponseCode {
