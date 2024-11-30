08565b1181232420fca870ec3014395d8c47c0e7
BaekSeungHyun7 <sergeant_keroro@naver.com>
2024-12-01 00:13:04 +0900

    TEST : 비활성화 테스트 1

--- src/test/java/com/gogym/GoGymApplicationTests.java
+++ src/test/java/com/gogym/GoGymApplicationTests.java
@@ -1,11 +1,14 @@
 package com.gogym;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+/*
 @SpringBootTest
 @ActiveProfiles("test")
+@Disabled("테스트 환경 설정 완료 전까지 비활성화")
 class GoGymApplicationTests {
 
 	@Test
@@ -13,3 +16,4 @@ class GoGymApplicationTests {
 	}
 
 }
+*/
