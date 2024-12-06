456eb50c09030d7cbd6ba2e3b9732af645dabf56
BaekSeungHyun7 <sergeant_keroro@naver.com>
2024-12-06 12:10:30 +0900

    🔒️ Fix : 인증 버그 수정 1139

--- src/main/java/com/gogym/member/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/gogym/member/jwt/JwtAuthenticationFilter.java
@@ -15,51 +15,41 @@ import java.util.List;
 
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
   private final JwtTokenProvider jwtTokenProvider;
   private final List<String> exemptUrls;
-
+  
   @Override
   protected boolean shouldNotFilter(HttpServletRequest request) {
     String path = request.getRequestURI();
-    // 인증 제외 경로에 해당하면 필터를 건너뜁니다.
     return exemptUrls.stream().anyMatch(path::startsWith);
-    
-  }
+    }
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    
-    try {
-      // 요청 헤더에서 JWT 토큰 추출
-      String token = jwtTokenProvider.extractToken(request, null);
 
-      // 토큰 검증 및 SecurityContext 설정
-      if (token != null && jwtTokenProvider.validateToken(token)) {
-        Authentication authentication = jwtTokenProvider.getAuthentication(token);
-        
-        if (authentication != null) {
-          // SecurityContextHolder에 인증 정보 설정
-          UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-              authentication.getPrincipal(),
-              null,
-              authentication.getAuthorities()
-              );
-          
-          authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-          SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-          }
-        
+    // 요청 헤더에서 JWT 토큰 추출
+    String token = jwtTokenProvider.extractToken(request, null);
+
+    // 토큰 검증 및 SecurityContext 설정
+    if (token != null && jwtTokenProvider.validateToken(token)) {
+      Authentication authentication = jwtTokenProvider.getAuthentication(token);
+
+      if (authentication != null) {
+        // SecurityContextHolder에 인증 정보 설정
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+            authentication.getPrincipal(),
+            null,
+            authentication.getAuthorities()
+        );
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
       }
-      
-    } catch (Exception e) {
-      // 예외 발생 시 SecurityContext를 초기화하여 보호
-      SecurityContextHolder.clearContext();
-      
     }
+
     // 다음 필터 체인으로 요청 전달
     filterChain.doFilter(request, response);
-    }
+  }
 }
-
-
