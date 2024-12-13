830d4023beea63f6e1933b8df23f690cf01d2b2b
Rud2K <rudgh2647k@naver.com>
2024-12-13 14:30:33 +0900

    ♻️ refactor: 채팅방 목록 조회 엔드포인트 매개변수 타입을 Pageable으로 받도록 수정

--- src/main/java/com/gogym/chat/controller/ChatRoomController.java
+++ src/main/java/com/gogym/chat/controller/ChatRoomController.java
@@ -1,6 +1,7 @@
 package com.gogym.chat.controller;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.gogym.chat.dto.ChatRoomDto.ChatRoomResponse;
 import com.gogym.chat.dto.ChatRoomDto.LeaveRequest;
@@ -53,9 +53,8 @@ public class ChatRoomController {
   @GetMapping
   public ResponseEntity<Page<ChatRoomResponse>> getChatRooms(
       @LoginMemberId Long memberId,
-      @RequestParam("page") int page,
-      @RequestParam("size") int size) {
-    return ResponseEntity.ok(this.chatRoomService.getChatRooms(memberId, page, size));
+      Pageable pageable) {
+    return ResponseEntity.ok(this.chatRoomService.getChatRooms(memberId, pageable));
   }
   
   /**
