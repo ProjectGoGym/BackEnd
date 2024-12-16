89cbc4ab48672da36f5699054090d0648d623d24
jeon-yuno <zero0577@naver.com>
2024-12-16 18:00:23 +0900

    ✨feat : 게시글 상태를 반환하는 메서드 작성, 잘못된 import refactor

--- src/main/java/com/gogym/post/repository/PostRepository.java
+++ src/main/java/com/gogym/post/repository/PostRepository.java
@@ -2,12 +2,13 @@ package com.gogym.post.repository;
 
 import com.gogym.post.entity.Post;
 import com.gogym.post.type.PostStatus;
-import io.lettuce.core.dynamic.annotation.Param;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
 
@@ -18,4 +19,7 @@ public interface PostRepository extends JpaRepository<Post, Long>, PostRepositor
       Pageable sortedByDate, @Param("regionIds") List<Long> regionIds);
 
   Page<Post> findByAuthorIdOrderByCreatedAtDesc(Long authorId, Pageable pageable);
+
+  @Query("SELECT p.status FROM Post p WHERE p.id = :postId")
+  Optional<PostStatus> findStatusByPostId(@Param("postId") Long postId);
 }
