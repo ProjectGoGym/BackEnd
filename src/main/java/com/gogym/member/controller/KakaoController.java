f575297a799e6c715906300bae476b66499255e3
BaekSeungHyun7 <114229031+BaekSeungHyun7@users.noreply.github.com>
2024-12-20 05:29:16 +0900

    ♻️ refactor: 카카오 로그인 리팩토링

--- src/main/java/com/gogym/member/controller/KakaoController.java
+++ src/main/java/com/gogym/member/controller/KakaoController.java
@@ -18,13 +18,21 @@ public class KakaoController {
   private final KakaoService kakaoService;
 
   @GetMapping("/sign-in")
-  public ResponseEntity<Void> handleKakaoLogin(@RequestParam("code") String code,
+  public ResponseEntity<Object> handleKakaoLogin(@RequestParam("code") String code,
       HttpServletRequest request) {
     String currentDomain = request.getRequestURL().toString().replace(request.getRequestURI(), "");
+
+    // 카카오 로그인 처리
     String token = kakaoService.processKakaoLogin(code, currentDomain);
 
+    if (token == null) {
+      // 회원 정보가 없거나 isKakao == false: 클라이언트에 false 반환
+      return ResponseEntity.ok(false);
+    }
+
+    // 토큰 발행 성공: 클라이언트에 true와 함께 토큰 전달
     HttpHeaders headers = new HttpHeaders();
     headers.add("Authorization", "Bearer " + token);
-    return ResponseEntity.ok().headers(headers).build();
+    return ResponseEntity.ok().headers(headers).body(true);
   }
 }
