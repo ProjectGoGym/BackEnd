58ee3313ba9751fbd7b00de9458b6b7d81ead457
Rud2K <rudgh2647k@naver.com>
2024-12-20 19:43:15 +0900

    ✨ feat: ChatMessage 엔티티 및 DTO에 MessageType 컬럼 및 필드 추가

--- src/main/java/com/gogym/chat/dto/ChatMessageDto.java
+++ src/main/java/com/gogym/chat/dto/ChatMessageDto.java
@@ -2,6 +2,7 @@ package com.gogym.chat.dto;
 
 import java.time.LocalDateTime;
 import org.springframework.data.domain.Page;
+import com.gogym.chat.type.MessageType;
 import com.gogym.post.type.PostStatus;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -21,11 +22,13 @@ public class ChatMessageDto {
       Long chatRoomId,
       Long senderId,
       String content,
+      MessageType messageType,
       LocalDateTime createdAt) {}
 
   public record RedisChatMessage(
       String content,
       Long senderId,
+      MessageType messageType,
       LocalDateTime createdAt) {}
 
   public record ChatRoomMessagesResponse(
