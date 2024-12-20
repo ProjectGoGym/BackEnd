414200576703057db0c2a1e64da7725b278bb209
BaekSeungHyun7 <114229031+BaekSeungHyun7@users.noreply.github.com>
2024-12-21 04:21:44 +0900

    ♻️ refactor: 시큐리티 설정 변경

--- src/main/java/com/gogym/config/SecurityConfig.java
+++ src/main/java/com/gogym/config/SecurityConfig.java
@@ -1,18 +1,18 @@
 package com.gogym.config;
 
+import com.gogym.member.jwt.JwtAuthenticationFilter;
+import com.gogym.member.jwt.JwtTokenProvider;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import com.gogym.member.jwt.JwtAuthenticationFilter;
-import com.gogym.member.jwt.JwtTokenProvider;
+import org.springframework.security.config.Customizer;
 
 @Configuration
 public class SecurityConfig {
@@ -40,17 +40,19 @@ public class SecurityConfig {
   // SecurityFilterChain Bean 등록
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf().disable().cors(Customizer.withDefaults()).authorizeHttpRequests(auth -> auth
-        // 인증 없이 접근을 허용할 엔드포인트
-        .requestMatchers("/api/auth/sign-up", "/api/auth/sign-in", "/api/auth/check-email",
-            "/api/auth/check-nickname", "/api/auth/verify-email", "/api/auth/reset-password",
-            "/api/auth/send-verification-email", "/api/regions", "/api/kakao/sign-in",
-            "/api/posts/views", "/api/posts/filters", "/api/posts/details/**",
-            "/api/payments/webhook", "/api/payments/sse/subscribe/**", "/api/images/presigned-url",
-            "/ws/**", "/api/notifications/subscribe/**")
-        .permitAll()
-        // 그 외의 모든 요청은 인증 필요
-        .anyRequest().authenticated())
+    http.csrf().disable().cors(Customizer.withDefaults())
+    .authorizeHttpRequests(auth -> auth
+            // 인증 없이 접근을 허용할 엔드포인트
+            .requestMatchers("/api/auth/sign-up", "/api/auth/sign-in", "/api/auth/check-email",
+                "/api/auth/check-nickname", "/api/auth/verify-email", "/api/auth/reset-password",
+                "/api/auth/send-verification-email", "/api/regions", "/api/kakao/sign-in",
+                "/api/posts/views", "/api/posts/filters", "/api/posts/details/**",
+                "/api/payments/webhook",
+                "/api/payments/sse/subscribe/**", "/api/images/presigned-url", "/ws/**",
+                "/api/notifications/subscribe/**")
+            .permitAll()
+            // 그 외의 모든 요청은 인증 필요
+            .anyRequest().authenticated())
         // JWT 인증 필터를 AuthenticationFilter 전에 추가
         .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
