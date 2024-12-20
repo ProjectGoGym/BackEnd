3dddf4481e12f4e6294eebb4d28c985427587c8b
BaekSeungHyun7 <114229031+BaekSeungHyun7@users.noreply.github.com>
2024-12-20 20:36:42 +0900

    ♻️ refactor: 멤버 엔티티 충돌해결

--- src/main/java/com/gogym/member/entity/Member.java
+++ src/main/java/com/gogym/member/entity/Member.java
@@ -2,10 +2,33 @@ package com.gogym.member.entity;
 
 import com.gogym.common.entity.BaseEntity;
 import com.gogym.gympay.entity.GymPay;
+import com.gogym.gympay.entity.Payment;
+import com.gogym.gympay.entity.Transaction;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import com.gogym.member.type.MemberStatus;
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 @Entity
 @Table(name = "members")
@@ -38,6 +61,7 @@ public class Member extends BaseEntity {
   private String password;
 
   @Enumerated(EnumType.STRING)
+  @JdbcTypeCode(SqlTypes.VARCHAR)
   @Column(name = "role", nullable = false)
   private Role role;
 
@@ -50,7 +74,7 @@ public class Member extends BaseEntity {
 
   @Column(name = "region_id_2", nullable = true)
   private Long regionId2;
-
+  
   @Setter
   @OneToOne(cascade = CascadeType.ALL)
   @JoinColumn(name = "gym_pay_id", referencedColumnName = "id")
@@ -60,9 +84,18 @@ public class Member extends BaseEntity {
   @Column(name = "verified_at")
   private LocalDateTime verifiedAt; // 이메일 인증 시간
 
-  @Setter
-  @Column(name = "is_kakao")
-  private boolean isKakao;
+  @Column(name = "is_kakao", nullable = false)
+  @Builder.Default
+  private Boolean isKakao = false; // 카카오 로그인 여부
+
+  @OneToMany(mappedBy = "member", cascade = CascadeType.PERSIST)
+  private final List<Payment> payments = new ArrayList<>();
+
+  @OneToMany(mappedBy = "seller")
+  private final List<Transaction> salesTransactions = new ArrayList<>();
+
+  @OneToMany(mappedBy = "buyer")
+  private final List<Transaction> purchaseTransactions = new ArrayList<>();
 
   // 이메일 인증 여부 확인 메서드
   public boolean isVerified() {
@@ -78,4 +111,3 @@ public class Member extends BaseEntity {
   }
 
 }
-
